988b23e7bab93f0b46606411c0b99083ccd8a68e
Daniel Londoño <daniel.esloh@gmail.com>
2024-06-30 02:37:39 -0500

    The basics for logging in were created.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 Rails.application.routes.draw do
   namespace :authentication, as: '', path: '' do
     resources :users, only: %i[new create], path: '/register', path_names: { new: '/' }
+    resources :sessions, only: %i[new create destroy], path: '/login', path_names: { new: '/'}
   end
   root 'home#welcome'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
